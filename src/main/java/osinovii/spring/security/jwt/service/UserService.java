fece56e74fdb3b688c5c60262ea7a6ae37721d0e
OsinoviAlex43 <osinovyjaleksej@gmail.com>
2025-05-10 17:01:48 +0300

    Creating package service and adding UserService logic

---
+++ src/main/java/osinovii/spring/security/jwt/service/UserService.java
@@ -0,0 +1,47 @@
+package osinovii.spring.security.jwt.service;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import osinovii.spring.security.jwt.entities.User;
+import osinovii.spring.security.jwt.repositories.RoleRepository;
+import osinovii.spring.security.jwt.repositories.UserRepository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class UserService implements UserDetailsService {
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+
+    public Optional<User> findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException(
+                String.format("User '%s' not found", username)));
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                user.getRoles().stream().map(role -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList())
+        );
+    }
+
+        public void createNewUser(User user){
+            user.setRoles(List.of(roleRepository.findByName("ROLE_USER").get()));
+            userRepository.save(user);
+        }
+    }
+
