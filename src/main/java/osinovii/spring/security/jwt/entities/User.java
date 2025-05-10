4d44a562e13826f2abd8e201c7a6b51d332739e9
OsinoviAlex43 <osinovyjaleksej@gmail.com>
2025-05-10 16:19:17 +0300

    Add properties,create H2 database,add Controllers and Entities

---
+++ src/main/java/osinovii/spring/security/jwt/entities/User.java
@@ -0,0 +1,19 @@
+package osinovii.spring.security.jwt.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "username")
+    private String username;
+
+
+}
